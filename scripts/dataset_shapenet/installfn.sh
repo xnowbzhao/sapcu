93c367c0ae531c9817ba36d70729bd12841c85f8
xnowbzhao <277876397@qq.com>
2022-10-31 18:43:23 +0800

    Update installfn.sh

--- scripts/dataset_shapenet/installfn.sh
+++ scripts/dataset_shapenet/installfn.sh
@@ -23,7 +23,7 @@ mkdir -p $OUTPUT_PATH
 for c in ${CLASSES[@]}; do
   echo "Parsing class $c"
   BUILD_PATH_C=$BUILD_PATH/$c
-  OUTPUT_PATH_C=/userhome/apu-r/data/ShapeNet/$c
+  OUTPUT_PATH_C=$OUTPUT_PATH/$c
   mkdir -p $OUTPUT_PATH_C
   echo $BUILD_PATH_C
   ls $BUILD_PATH_C/4_fn/ | sed -e 's/\.npz$//' | parallel -P $NPROC --timeout $TIMEOUT \
